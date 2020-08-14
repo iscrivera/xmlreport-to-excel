7ac7f5bcb1d089a43fee88db4351418f30d02252
ASCENCION RIVERA VELASQUEZ <C262780@santander.com.mx>
2020-08-14 15:04:10 -0500

    Initial commit

---
+++ src/test/java/mx/com/iscrivera/convert/XmlreportToExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.iscrivera.convert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlreportToExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
